2467dec9dd4d3fce191c7f3160d13197133f4807
Gabs-Moura <gabrielmourasouza@hotmail.com>
2025-06-11 19:17:47 -0300

    OMG-50 - adding garrafinha_agua dataset

---
+++ modificador.py
@@ -0,0 +1,24 @@
+import os
+
+# Caminho da pasta onde estão os arquivos .txt
+pasta = '/Users/moura/Downloads/garrafinha_agua/valid/labels'
+
+for nome_arquivo in os.listdir(pasta):
+    if nome_arquivo.endswith('.txt'):
+        caminho_arquivo = os.path.join(pasta, nome_arquivo)
+        
+        with open(caminho_arquivo, 'r') as arquivo:
+            linhas = arquivo.readlines()
+        
+        novas_linhas = []
+        for linha in linhas:
+            if linha and linha[0] in '123456':
+                nova_linha = '2' + linha[1:]
+            else:
+                nova_linha = linha
+            novas_linhas.append(nova_linha)
+        
+        with open(caminho_arquivo, 'w') as arquivo:
+            arquivo.writelines(novas_linhas)
+
+print("Todos os arquivos foram processados.")
